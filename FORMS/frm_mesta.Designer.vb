c0e399b78ad2b1743ec49b175ab7eca98041f393
Alex <Alex@Zorg>
2014-07-02 22:45:25 +0400

    ?????? 1.7.5.2 beta

---
+++ FORMS/frm_mesta.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_mesta
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lvReport2Cl = New System.Windows.Forms.ListView()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lvReport2Cl
+        '
+        Me.lvReport2Cl.Dock = System.Windows.Forms.DockStyle.Top
+        Me.lvReport2Cl.FullRowSelect = True
+        Me.lvReport2Cl.GridLines = True
+        Me.lvReport2Cl.Location = New System.Drawing.Point(0, 0)
+        Me.lvReport2Cl.Margin = New System.Windows.Forms.Padding(30)
+        Me.lvReport2Cl.MultiSelect = False
+        Me.lvReport2Cl.Name = "lvReport2Cl"
+        Me.lvReport2Cl.ShowItemToolTips = True
+        Me.lvReport2Cl.Size = New System.Drawing.Size(650, 489)
+        Me.lvReport2Cl.TabIndex = 18
+        Me.lvReport2Cl.UseCompatibleStateImageBehavior = False
+        Me.lvReport2Cl.View = System.Windows.Forms.View.Details
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(561, 510)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 20
+        Me.Button2.Text = "В офис"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 510)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "Выход"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'frm_mesta
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(650, 545)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.lvReport2Cl)
+        Me.Name = "frm_mesta"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Места установки"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lvReport2Cl As System.Windows.Forms.ListView
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
